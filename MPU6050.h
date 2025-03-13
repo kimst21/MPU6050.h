d4c090e0c074924f5021205cf7982ed3b1d01ff6
kimst21 <118581137+kimst21@users.noreply.github.com>
2025-03-13 19:20:46 +0900

    Update MPU6050.h

--- MPU6050.h
+++ MPU6050.h
@@ -1,242 +1,133 @@
-//Header files
-#include "MPU6050.h"
+//Header Files
+#include "stm32f4xx_hal.h"  //dpending on your board
+#include <string.h>
+#include <stdbool.h>	//Boolean
+#include <math.h>			//Pow()
 
-//Library Variable
-//1- I2C Handle
-static I2C_HandleTypeDef i2cHandler;
-//2- Accel & Gyro Scaling Factor
-static float accelScalingFactor, gyroScalingFactor;
-//3- Bias varaibles
-static float A_X_Bias = 0.0f;
-static float A_Y_Bias = 0.0f;
-static float A_Z_Bias = 0.0f;
+//Define Registers
+#define WHO_AM_I_REG			0x75
+#define MPU_ADDR					0x69
+#define PWR_MAGT_1_REG		0x6B
+#define CONFIG_REG				0x1A
+#define GYRO_CONFIG_REG		0x1B
+#define ACCEL_CONFIG_REG	0x1C
+#define SMPLRT_DIV_REG		0x19
+#define INT_STATUS_REG		0x3A
+#define ACCEL_XOUT_H_REG	0x3B
+#define TEMP_OUT_H_REG		0x41
+#define GYRO_XOUT_H_REG		0x43
+#define FIFO_EN_REG 			0x23
+#define INT_ENABLE_REG 		0x38
+#define I2CMACO_REG 			0x23
+#define USER_CNT_REG			0x6A
+#define FIFO_COUNTH_REG 	0x72
+#define FIFO_R_W_REG 			0x74
 
-static int16_t GyroRW[3];
+//TypeDefs and Enums
+//1- MPU Configuration
+typedef struct
+{
+	uint8_t ClockSource;
+	uint8_t Gyro_Full_Scale;
+	uint8_t Accel_Full_Scale;
+	uint8_t CONFIG_DLPF;
+	bool 		Sleep_Mode_Bit;
 
-//Fucntion Definitions
+}MPU_ConfigTypeDef;
+//2- Clock Source ENUM
+enum PM_CLKSEL_ENUM
+{
+	Internal_8MHz 	= 0x00,
+	X_Axis_Ref			= 0x01,
+	Y_Axis_Ref			= 0x02,
+	Z_Axis_Ref			= 0x03,
+	Ext_32_768KHz		= 0x04,
+	Ext_19_2MHz			= 0x05,
+	TIM_GENT_INREST	= 0x07
+};
+//3- Gyro Full Scale Range ENUM (deg/sec)
+enum gyro_FullScale_ENUM
+{
+	FS_SEL_250 	= 0x00,
+	FS_SEL_500 	= 0x01,
+	FS_SEL_1000 = 0x02,
+	FS_SEL_2000	= 0x03
+};
+//4- Accelerometer Full Scale Range ENUM (1g = 9.81m/s2)
+enum accel_FullScale_ENUM
+{
+	AFS_SEL_2g	= 0x00,
+	AFS_SEL_4g,
+	AFS_SEL_8g,
+	AFS_SEL_16g
+};
+//5- Digital Low Pass Filter ENUM
+enum DLPF_CFG_ENUM
+{
+	DLPF_260A_256G_Hz = 0x00,
+	DLPF_184A_188G_Hz = 0x01,
+	DLPF_94A_98G_Hz 	= 0x02,
+	DLPF_44A_42G_Hz 	= 0x03,
+	DLPF_21A_20G_Hz 	= 0x04,
+	DLPF_10_Hz 				= 0x05,
+	DLPF_5_Hz 				= 0x06
+};
+//6- e external Frame Synchronization ENUM
+enum EXT_SYNC_SET_ENUM
+{
+	input_Disable = 0x00,
+	TEMP_OUT_L		= 0x01,
+	GYRO_XOUT_L		= 0x02,
+	GYRO_YOUT_L		= 0x03,
+	GYRO_ZOUT_L		= 0x04,
+	ACCEL_XOUT_L	= 0x05,
+	ACCEL_YOUT_L	= 0x06,
+	ACCEL_ZOUT_L	= 0x07
+};
+
+//7. Raw data typedef
+typedef struct
+{
+	int16_t x;
+	int16_t y;
+	int16_t z;
+}RawData_Def;
+
+//8. Scaled data typedef
+typedef struct
+{
+	float x;
+	float y;
+	float z;
+}ScaledData_Def;
+
+
+//Function Prototype
 //1- i2c Handler
-void MPU6050_Init(I2C_HandleTypeDef *I2Chnd)
-{
-	//Copy I2C CubeMX handle to local library
-	memcpy(&i2cHandler, I2Chnd, sizeof(*I2Chnd));
-}
-
+void MPU6050_Init(I2C_HandleTypeDef *I2Chnd);
 //2- i2c Read
-void I2C_Read(uint8_t ADDR, uint8_t *i2cBif, uint8_t NofData)
-{
-	uint8_t i2cBuf[2];
-	uint8_t MPUADDR;
-	//Need to Shift address to make it proper to i2c operation
-	MPUADDR = (MPU_ADDR<<1);
-	i2cBuf[0] = ADDR;
-	HAL_I2C_Master_Transmit(&i2cHandler, MPUADDR, i2cBuf, 1, 10);
-	HAL_I2C_Master_Receive(&i2cHandler, MPUADDR, i2cBif, NofData, 100);
-}
-
-//3- i2c Write
-void I2C_Write8(uint8_t ADDR, uint8_t data)
-{
-	uint8_t i2cData[2];
-	i2cData[0] = ADDR;
-	i2cData[1] = data;
-	uint8_t MPUADDR = (MPU_ADDR<<1);
-	HAL_I2C_Master_Transmit(&i2cHandler, MPUADDR, i2cData, 2,100);
-}
-
+void I2C_Read(uint8_t ADDR, uint8_t *i2cBuf, uint8_t NofData);
+//3- i2c Write 8 Bit
+void I2C_Write8(uint8_t ADDR, uint8_t data);
 //4- MPU6050 Initialaztion Configuration
-void MPU6050_Config(MPU_ConfigTypeDef *config)
-{
-	uint8_t Buffer = 0;
-	//Clock Source
-	//Reset Device
-	I2C_Write8(PWR_MAGT_1_REG, 0x80);
-	HAL_Delay(100);
-	Buffer = config ->ClockSource & 0x07; //change the 7th bits of register
-	Buffer |= (config ->Sleep_Mode_Bit << 6) &0x40; // change only the 7th bit in the register
-	I2C_Write8(PWR_MAGT_1_REG, Buffer);
-	HAL_Delay(100); // should wait 10ms after changeing the clock setting.
-
-	//Set the Digital Low Pass Filter
-	Buffer = 0;
-	Buffer = config->CONFIG_DLPF & 0x07;
-	I2C_Write8(CONFIG_REG, Buffer);
-
-	//Select the Gyroscope Full Scale Range
-	Buffer = 0;
-	Buffer = (config->Gyro_Full_Scale << 3) & 0x18;
-	I2C_Write8(GYRO_CONFIG_REG, Buffer);
-
-	//Select the Accelerometer Full Scale Range
-	Buffer = 0;
-	Buffer = (config->Accel_Full_Scale << 3) & 0x18;
-	I2C_Write8(ACCEL_CONFIG_REG, Buffer);
-	//Set SRD To Default
-	MPU6050_Set_SMPRT_DIV(0x04);
-
-
-	//Accelerometer Scaling Factor, Set the Accelerometer and Gyroscope Scaling Factor
-	switch (config->Accel_Full_Scale)
-	{
-		case AFS_SEL_2g:
-			accelScalingFactor = (2000.0f/32768.0f);
-			break;
-
-		case AFS_SEL_4g:
-			accelScalingFactor = (4000.0f/32768.0f);
-				break;
-
-		case AFS_SEL_8g:
-			accelScalingFactor = (8000.0f/32768.0f);
-			break;
-
-		case AFS_SEL_16g:
-			accelScalingFactor = (16000.0f/32768.0f);
-			break;
-
-		default:
-			break;
-	}
-	//Gyroscope Scaling Factor
-	switch (config->Gyro_Full_Scale)
-	{
-		case FS_SEL_250:
-			gyroScalingFactor = 250.0f/32768.0f;
-			break;
-
-		case FS_SEL_500:
-				gyroScalingFactor = 500.0f/32768.0f;
-				break;
-
-		case FS_SEL_1000:
-			gyroScalingFactor = 1000.0f/32768.0f;
-			break;
-
-		case FS_SEL_2000:
-			gyroScalingFactor = 2000.0f/32768.0f;
-			break;
-
-		default:
-			break;
-	}
-
-}
-
+void MPU6050_Config(MPU_ConfigTypeDef *config);
 //5- Get Sample Rate Divider
-uint8_t MPU6050_Get_SMPRT_DIV(void)
-{
-	uint8_t Buffer = 0;
-
-	I2C_Read(SMPLRT_DIV_REG, &Buffer, 1);
-	return Buffer;
-}
-
+uint8_t MPU6050_Get_SMPRT_DIV(void);
 //6- Set Sample Rate Divider
-void MPU6050_Set_SMPRT_DIV(uint8_t SMPRTvalue)
-{
-	I2C_Write8(SMPLRT_DIV_REG, SMPRTvalue);
-}
-
-//7- Get External Frame Sync.
-uint8_t MPU6050_Get_FSYNC(void)
-{
-	uint8_t Buffer = 0;
-
-	I2C_Read(CONFIG_REG, &Buffer, 1);
-	Buffer &= 0x38;
-	return (Buffer>>3);
-}
-
+void MPU6050_Set_SMPRT_DIV(uint8_t SMPRTvalue);
+//7- External Frame Sync.
+uint8_t MPU6050_Get_FSYNC(void);
 //8- Set External Frame Sync.
-void MPU6050_Set_FSYNC(enum EXT_SYNC_SET_ENUM ext_Sync)
-{
-	uint8_t Buffer = 0;
-	I2C_Read(CONFIG_REG, &Buffer,1);
-	Buffer &= ~0x38;
-
-	Buffer |= (ext_Sync <<3);
-	I2C_Write8(CONFIG_REG, Buffer);
-
-}
-
+void MPU6050_Set_FSYNC(enum EXT_SYNC_SET_ENUM ext_Sync);
 //9- Get Accel Raw Data
-void MPU6050_Get_Accel_RawData(RawData_Def *rawDef)
-{
-	uint8_t i2cBuf[2];
-	uint8_t AcceArr[6], GyroArr[6];
-
-	I2C_Read(INT_STATUS_REG, &i2cBuf[1],1);
-	if((i2cBuf[1]&&0x01))
-	{
-		I2C_Read(ACCEL_XOUT_H_REG, AcceArr,6);
-
-		//Accel Raw Data
-		rawDef->x = ((AcceArr[0]<<8) + AcceArr[1]); // x-Axis
-		rawDef->y = ((AcceArr[2]<<8) + AcceArr[3]); // y-Axis
-		rawDef->z = ((AcceArr[4]<<8) + AcceArr[5]); // z-Axis
-		//Gyro Raw Data
-		I2C_Read(GYRO_XOUT_H_REG, GyroArr,6);
-		GyroRW[0] = ((GyroArr[0]<<8) + GyroArr[1]);
-		GyroRW[1] = (GyroArr[2]<<8) + GyroArr[3];
-		GyroRW[2] = ((GyroArr[4]<<8) + GyroArr[5]);
-	}
-}
-
-//10- Get Accel scaled data (g unit of gravity, 1g = 9.81m/s2)
-void MPU6050_Get_Accel_Scale(ScaledData_Def *scaledDef)
-{
-
-	RawData_Def AccelRData;
-	MPU6050_Get_Accel_RawData(&AccelRData);
-
-	//Accel Scale data
-	scaledDef->x = ((AccelRData.x+0.0f)*accelScalingFactor);
-	scaledDef->y = ((AccelRData.y+0.0f)*accelScalingFactor);
-	scaledDef->z = ((AccelRData.z+0.0f)*accelScalingFactor);
-}
-
+void MPU6050_Get_Accel_RawData(RawData_Def *rawDef);//************
+//10- Get Accel scaled data
+void MPU6050_Get_Accel_Scale(ScaledData_Def *scaledDef);//***********
 //11- Get Accel calibrated data
-void MPU6050_Get_Accel_Cali(ScaledData_Def *CaliDef)
-{
-	ScaledData_Def AccelScaled;
-	MPU6050_Get_Accel_Scale(&AccelScaled);
-
-	//Accel Scale data
-	CaliDef->x = (AccelScaled.x) - A_X_Bias; // x-Axis
-	CaliDef->y = (AccelScaled.y) - A_Y_Bias;// y-Axis
-	CaliDef->z = (AccelScaled.z) - A_Z_Bias;// z-Axis
-}
+void MPU6050_Get_Accel_Cali(ScaledData_Def *CaliDef);
 //12- Get Gyro Raw Data
-void MPU6050_Get_Gyro_RawData(RawData_Def *rawDef)
-{
-
-	//Accel Raw Data
-	rawDef->x = GyroRW[0];
-	rawDef->y = GyroRW[1];
-	rawDef->z = GyroRW[2];
-
-}
-
+void MPU6050_Get_Gyro_RawData(RawData_Def *rawDef);
 //13- Get Gyro scaled data
-void MPU6050_Get_Gyro_Scale(ScaledData_Def *scaledDef)
-{
-	RawData_Def myGyroRaw;
-	MPU6050_Get_Gyro_RawData(&myGyroRaw);
-
-	//Gyro Scale data
-	scaledDef->x = (myGyroRaw.x)*gyroScalingFactor; // x-Axis
-	scaledDef->y = (myGyroRaw.y)*gyroScalingFactor; // y-Axis
-	scaledDef->z = (myGyroRaw.z)*gyroScalingFactor; // z-Axis
-}
-
+void MPU6050_Get_Gyro_Scale(ScaledData_Def *scaledDef);
 //14- Accel Calibration
-void _Accel_Cali(float x_min, float x_max, float y_min, float y_max, float z_min, float z_max)
-{
-	//1* X-Axis calibrate
-	A_X_Bias		= (x_max + x_min)/2.0f;
-
-	//2* Y-Axis calibrate
-	A_Y_Bias		= (y_max + y_min)/2.0f;
-
-	//3* Z-Axis calibrate
-	A_Z_Bias		= (z_max + z_min)/2.0f;
-}
+void _Accel_Cali(float x_min, float x_max, float y_min, float y_max, float z_min, float z_max);
